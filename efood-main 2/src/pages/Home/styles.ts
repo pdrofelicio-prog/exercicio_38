5f8a40f59ed7c89ec42fcbdef46b6e795584b09b
pdrofelicio-prog <pedro.felicio.silva@gmail.com>
2024-09-12 11:33:57 -0300

    Delete efood-main 2 directory

--- efood-main 2/src/pages/Home/styles.ts
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components'
-import theme from '../../global/theme'
-
-export const HomeContainer = styled.main`
-  background-color: ${theme.Colors.background};
-`
--- efood-main 2/src/pages/Perfil/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-import theme from '../../global/theme'
-
-export const PerfilContainer = styled.main`
-  background-color: ${theme.Colors.background};
-  position: relative;
-`
